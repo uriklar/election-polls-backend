2a7c1b2cede972e3b96701cccda931bc93be0528
Rubysoft <uriklar@gmail.com>
2015-01-09 14:33:44 +0200

    destroy result with poll

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,4 +1,4 @@
 class Poll < ActiveRecord::Base
-	has_many :results, -> { order 'mandates desc' }
+	has_many :results, :dependent => :destroy, -> { order 'mandates desc' }
 	accepts_nested_attributes_for :results
 end
