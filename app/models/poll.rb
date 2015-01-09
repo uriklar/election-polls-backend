d32d9113366cb29122bb9188fa1bff65f583df4e
Rubysoft <uriklar@gmail.com>
2015-01-09 14:38:17 +0200

    fixed error

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,4 +1,4 @@
 class Poll < ActiveRecord::Base
-	has_many :results, :dependent => :destroy, -> { order 'mandates desc' }
+	has_many :results, -> { order 'mandates desc' }, :dependent => :destroy
 	accepts_nested_attributes_for :results
 end
